49786fbbec29f0c8618cdac145ba9a416f528c3a
Sam Clements <sam@borntyping.co.uk>
2014-02-07 14:48:06 +0000

    Added timer to avoid running command repeatedly

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name = "watch-fs",
-    version = '0.1.2',
+    version = '0.2.0',
 
     author = "Sam Clements",
     author_email = "sam@borntyping.co.uk",
--- watch_fs.py
+++ watch_fs.py
@@ -3,6 +3,7 @@ watch-fs is a command line tool to run commands when files change
 """
 
 import argparse
+import datetime
 import subprocess
 
 import pyinotify
@@ -16,22 +17,52 @@ parser.add_argument(
     '-d', action='append', metavar='DIR', dest='paths',
     help="A directory to watch. Can be used more than once.")
 
-class Command(object):
-    def __init__(self, command):
-        self.command = command
 
-    def __call__(self, event):
-        if event.dir:
-            return
+class Timer(object):
+    """Call a function if $delay has passed since the last call"""
+
+    @staticmethod
+    def now():
+        return datetime.datetime.now()
+
+    def __init__(self, delay, function):
+        self.delay = datetime.timedelta(seconds=delay)
+        self.function = function
+        self.set_last_call_time()
+
+    def set_last_call_time(self):
+        self.last_call = self.now()
+
+    def time_since_last_call(self):
+        return self.now() - self.last_call
+
+    def __call__(self, *args, **kwargs):
+        if self.time_since_last_call() > self.delay:
+            self.function(*args, **kwargs)
+            self.set_last_call_time()
+
+
+class Command(pyinotify.ProcessEvent):
+    def __init__(self, command, delay=0.5):
+        self.command = command
+        self.timer = Timer(delay, self.run_command)
+
+    def run_command(self, event):
         command = self.command.format(name=event.name, path=event.pathname)
         proccess = subprocess.Popen(command, shell=True)
         proccess.wait()
+
+    def process_default(self, event):
+        if not event.dir:
+            self.timer(event)
+
 
 def main(mask=DEFAULT_INOTIFY_MASK):
     args = parser.parse_args()
 
     watch_manager = pyinotify.WatchManager()
-    notifier = pyinotify.Notifier(watch_manager, Command(args.command))
+    event_handler = Command(args.command)
+    notifier = pyinotify.Notifier(watch_manager, event_handler)
 
     for path in (args.paths or ['.']):
         watch_manager.add_watch(path, mask, rec=True, auto_add=True)
