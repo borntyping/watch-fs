0c0756895f50203b1ea55f2fa6a52af3634e4d4e
Sam Clements <sam@borntyping.co.uk>
2014-10-14 12:11:02 +0100

    Finish rewrite

--- watch_fs.py
+++ watch_fs.py
@@ -9,7 +9,7 @@ import pyinotify
 
 @click.command()
 @click.option(
-    '--directory', '-d', 'directories',
+    '--directory', '-d', 'directories', multiple=True, default=['.'],
     type=click.Path(dir_okay=True, file_okay=False, exists=True),
     help="A directory to watch for file changes - can be used multiple times, "
     "and defaults to the current directory.")
@@ -17,7 +17,7 @@ import pyinotify
     '-c', '--clear/--no-clear', default=False,
     help="Clear the terminal before running the command")
 @click.option(
-    '-D', '--delay', type=click.FLOAT, default=1,
+    '-D', '--delay', type=click.FLOAT, default=0.5,
     help="Minium delay before running the command again, in seconds")
 @click.option(
     '-v', '--verbose/--quiet', default=False,
@@ -27,28 +27,53 @@ def main(directories, clear, delay, verbose, command):
     WatchFS(directories, command, clear, delay, verbose=True).run()
 
 
+class Timer(object):
+    """Call a function if $delay has passed since the last successful call"""
+
+    @staticmethod
+    def now():
+        return datetime.datetime.now()
+
+    def __init__(self, delay):
+        self.delay = datetime.timedelta(seconds=delay)
+        self.finished_at = self.now() - self.delay
+
+    def ready(self):
+        """Returns True if .delay has passed since .finished_at"""
+        return (self.now() - self.finished_at) > self.delay
+
+    def finished(self):
+        """Resets the finished_at attribute"""
+        self.finished_at = self.now()
+
+
 class WatchFS(pyinotify.ProcessEvent):
-    def __init__(self, directories, command,
-                 clear=False, delay=0.5, verbose=True,
+    def __init__(self, directories, command, clear, delay, verbose,
                  mask=pyinotify.IN_CREATE | pyinotify.IN_MODIFY):
-        self.directories = directories or ['.']
+        self.directories = directories
         self.command = command
         self.clear = clear
-        # self.delay = delay
         self.verbose = verbose
         self.mask = mask
-
         self.timer = Timer(delay)
 
     def run(self):
+        """Setup inotify to call run_command() on each event"""
         watch_manager = pyinotify.WatchManager()
         for d in self.directories:
             watch_manager.add_watch(d, self.mask, rec=True, auto_add=True)
-        notifier = pyinotify.Notifier(watch_manager, self)
+        notifier = pyinotify.Notifier(
+            watch_manager, default_proc_fun=self.maybe_run_command)
         self.run_command()
         notifier.loop()
 
-    def run_command(self):
+    def maybe_run_command(self, event=None):
+        """Run the command *if* the delay has passed"""
+        if self.timer.ready():
+            self.run_command()
+            self.timer.finished()
+
+    def run_command(self, event=None):
         if self.clear:
             subprocess.call('clear')
 
@@ -57,30 +82,8 @@ class WatchFS(pyinotify.ProcessEvent):
 
         exit_code = subprocess.call(self.command, shell=True)
         if self.verbose and exit_code:
-            print("Command '{}' exited with code {}".format(
+            print("! Command '{}' exited with code {}".format(
                 self.command, exit_code))
-
-    def process_default(self, event):
-        if self.timer():
-            self.run_command()
-
-
-class Timer(object):
-    """Call a function if $delay has passed since the last successful call"""
-
-    @staticmethod
-    def now():
-        return datetime.datetime.now()
-
-    def __init__(self, delay=1):
-        self.delay = datetime.timedelta(seconds=delay)
-        self.last_call = self.now() - self.delay
-
-    def __call__(self):
-        if (self.now() - self.last_call) > self.delay:
-            self.last_call = self.now()
-            return True
-        return False
 
 if __name__ == '__main__':
     main()
