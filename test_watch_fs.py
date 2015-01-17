5e53dac6f4975d37a13310648a15ce17cd814981
Sam Clements <sam@borntyping.co.uk>
2015-01-17 17:30:20 +0000

    Added some basic tests

---
+++ test_watch_fs.py
@@ -0,0 +1,33 @@
+import watch_fs
+
+import click.testing
+import mock
+import pytest
+
+
+@pytest.fixture
+def run():
+    return lambda *args: click.testing.CliRunner().invoke(watch_fs.main, args)
+
+
+@mock.patch('pyinotify.Notifier.loop')
+class TestWatchFS(object):
+    def test_debug_output(self, loop_mock, run):
+        """Check the debug output"""
+        result = run('-vv', 'echo', 'hello', 'world')
+        assert result.output == (
+            '$ echo hello world\n'
+            '! Command \'echo hello world\' exited with code 0\n'
+        )
+
+    def test_loop_called(self, loop_mock, run):
+        run('echo')
+
+        assert isinstance(loop_mock, mock.Mock)
+        loop_mock.assert_called_once_with()
+
+    def test_run_command(self, loop_mock, run, capfd):
+        """Check that the echo command was run"""
+        run('echo', 'hello', 'world')
+        out, err = capfd.readouterr()
+        assert out.strip() == 'hello world'
